5e9f1274e366474be1d0711b1d0bfb794992f402
gulshan89 <gulsn.smdzad@mail.ru>
2019-08-12 20:26:50 -0700

    Ilk fayl

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// $(".dragitem").on("dragstart",function(e){
+//     e.originalEvent.dataTransfer.setData("gulshen",$(this).attr("id"))
+// })
+// $(".dropFiel").on("dragover",function(e){
+//     e.preventDefault()
+// })
+// $(".dropFiel").on("drop",function(e){
+//     if($(this).children().length<1){
+//         var data=e.originalEvent.dataTransfer.getData("gulshen");
+//         $(this).append($("#"+data))
+//     }
+// })
+
